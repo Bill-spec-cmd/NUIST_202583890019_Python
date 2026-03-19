4ab7d417d1585425faab302003940b431a927421
Bill-spec-cmd <2938382899@qq.com>
2026-03-19 19:31:01 +0800

    Update to Version 2: user input addition
    Updated the script to accept user input for numbers.

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,8 +1,7 @@
 # Add Two Numbers in Python
 # Author: 你的名字
-# Using the + Operator
-a = 15
-b = 12
-# Adding two numbers
-res = a + b
+# Using user input
+a = input("First number: ")
+b = input("Second number: ")
+res = float(a) + float(b)
 print(res)
